1477b43f41046569f373c5bb9e5eecf2f7ab2e06
romanyukovmaksim09-coder <romanyukovmaksim09@mail.ru>
2026-01-15 11:43:00 +0300

    Update Body.java
    добавлена физика взаимодействия со стенами
    исправлена ошибка направления тела в сторону мыши

--- CBO/src/Body.java
+++ CBO/src/Body.java
@@ -5,7 +5,7 @@ import java.io.File;
 import javax.imageio.ImageIO;
 
 class Body {
-    private int x, y; // Координаты точки C
+    private int x, y;
     private BufferedImage imageBody;
     private double lookAngle = 0;
     private int mouseX = 500;
@@ -13,7 +13,6 @@ class Body {
 
     private int bodyWidth, bodyHeight;
 
-    // ДОБАВЛЕНО: смещение для корректировки центра тела
     private final int BODY_CENTER_OFFSET = 50;
 
     public Body() throws Exception {
@@ -30,7 +29,6 @@ class Body {
             imageBody = ImageIO.read(heroFile);
             bodyWidth = imageBody.getWidth() / 3;
             bodyHeight = imageBody.getHeight() / 3;
-            //System.out.println("Тело: 3x меньше (" + bodyWidth + "x" + bodyHeight + ")");
         } else {
             throw new Exception("Файл hero.png не найден!");
         }
@@ -56,7 +54,6 @@ class Body {
     }
 
     public void setPositionFromLegs(int pointCX, int pointCY) {
-        // Тело рисуется точно в точке C
         this.x = pointCX;
         this.y = pointCY;
     }
@@ -65,7 +62,6 @@ class Body {
         Graphics2D g2d = (Graphics2D) g;
         AffineTransform originalTransform = g2d.getTransform();
 
-        // Рисуем тело в точке C
         g2d.translate(x, y);
         g2d.rotate(lookAngle);
 
@@ -73,38 +69,13 @@ class Body {
             int width = bodyWidth;
             int height = bodyHeight;
 
-            // ДОБАВЛЕНО: корректировка положения тела
-            // Сдвигаем тело вправо на 20 пикселей, чтобы компенсировать смещение центра
-            int drawX = -width/2 + BODY_CENTER_OFFSET;  // Смещение вправо
+            int drawX = -width/2 + BODY_CENTER_OFFSET;
             int drawY = -height/2;
 
             g2d.drawImage(imageBody, drawX, drawY, width, height, null);
-
-            // Отладочная точка центра тела (точка C)
-            drawDebugCenter(g2d);
         }
 
         g2d.setTransform(originalTransform);
-    }
-
-    private void drawDebugCenter(Graphics2D g2d) {
-        // Зеленая точка в центре тела (точка C)
-        g2d.setColor(Color.GREEN);
-        g2d.fillOval(-6, -6, 12, 12);
-
-        // Текст
-        g2d.setColor(Color.BLACK);
-        //g2d.drawString("Точка C", 10, -10);
-
-        // ДОБАВЛЕНО: линия, показывающая смещение
-        g2d.setColor(new Color(255, 0, 0, 150));
-        g2d.setStroke(new BasicStroke(2));
-        g2d.drawLine(0, 0, BODY_CENTER_OFFSET, 0);
-        g2d.setStroke(new BasicStroke(1));
-
-        // Текст смещения
-        g2d.setColor(Color.RED);
-        //g2d.drawString("Смещение: " + BODY_CENTER_OFFSET + "px", 25, -15);
     }
 
     public int getX() { return x; }
